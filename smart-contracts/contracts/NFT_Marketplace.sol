5f30bc2e98090823a9f4cb37d6d7a3d2ffa157ef
Sourasish01 <bhotto2023@gmail.com>
2025-03-30 16:56:09 +0530

    first commit, hardhat setup, openzepplin setup, imports to smartcontract

---
+++ smart-contracts/contracts/NFT_Marketplace.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+//is used to import Hardhat’s built-in debugging tool, console.log, which allows you to print debug messages while testing and deploying your smart contracts.
+//console.sol only works in Hardhat (not in production or testnets).It increases gas consumption (but it’s fine for debugging locally).
+//Remove it before deploying to production.
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract Lock {
+    
+}
